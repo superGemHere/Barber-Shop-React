45588398b87d73332d22ca6b5ef39be0b7071daf
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-12 23:26:04 +0200

    +Products Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header.jsx'
 import Home from './components/Home Template/Home.jsx';
 import Services from './components/Services Template/Services.jsx';
+import Products from './components/Products Template/Products.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/services' element={<Services />} />
+      <Route path='/products' element={<Products />} />
     </Routes>
     </>
   );
