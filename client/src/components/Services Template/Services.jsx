b90107db7932371712d0073cb2e8e29fa1b10c4d
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-12 21:07:44 +0200

    +services jsx

---
+++ client/src/components/Services Template/Services.jsx
@@ -0,0 +1,45 @@
+import './services.css'
+
+export default function Services(){
+
+    return (
+        <>
+        <main className="services color-white">
+    <div className="servicesParentContainer">
+        <p id="serviceHeading">Here are our services.</p>
+        <div className="servicesChildContainer">
+                <div className="serviceCard haircuts" >
+                                <h2>Haircuts</h2>
+                                <div className="cardInsights">
+                                    <h4>Haircuts</h4>
+                                    <p>Click for more details.</p>
+                                </div>
+                            </div>
+            
+            <div className="serviceCard beardTrimming" >
+                <h2>Beard Trimming</h2>
+                    <div className="cardInsights">
+                        <h4>Beard Trimming</h4>
+                        <p>Click for more details.</p>
+                    </div>
+            </div>
+            <div className="serviceCard" >
+                <h2>Wax</h2>
+                    <div className="cardInsights">
+                        <h4>Wax</h4>
+                        <p>Click for more details.</p>
+                    </div>
+            </div>
+            <div className="serviceCard">
+                <h2>Face Masks</h2>
+                    <div className="cardInsights">
+                        <h4>Face Masks</h4>
+                        <p>Click for more details.</p>
+                    </div>
+            </div>
+        </div>
+    </div>
+</main>
+</>
+    );
+}
