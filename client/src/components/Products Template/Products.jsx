225a20bfaf803981c3d203db523d58a6682452f1
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-12 23:29:13 +0200

    +Products Component

---
+++ client/src/components/Products Template/Products.jsx
@@ -0,0 +1,86 @@
+import './products.css'
+
+export default function Products(){
+
+
+    return(
+        <main className="products">
+        <div className="productsParentContainer">
+            <h2 id="productsHeading">Our Products:</h2>
+            <div className="productChildContainer">
+                <div className="productCard">
+                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
+                    <div className="cardDetails">
+                        <div className="insideCard">
+                            <p className="color-white">Matte Pomade</p>
+                            <p><span className="color-white">Price: $5.99</span></p>
+                        </div>
+                        <a href="#" id="detailsBtn">Details</a>
+                    </div>
+                </div>
+                <div className="productCard">
+                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
+                    <div className="cardDetails">
+                        <div className="insideCard">
+                            <p className="color-white">Matte Pomade</p>
+                            <p><span className="color-white">Price: $5.99</span></p>
+                        </div>
+                        <a href="#" id="detailsBtn">Details</a>
+                    </div>
+                </div>
+                <div className="productCard">
+                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
+                    <div className="cardDetails">
+                        <div className="insideCard">
+                            <p className="color-white">Matte Pomade</p>
+                            <p><span className="color-white">Price: $5.99</span></p>
+                        </div>
+                        <a href="#" id="detailsBtn">Details</a>
+                    </div>
+                </div>
+                <div className="productCard">
+                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
+                    <div className="cardDetails">
+                        <div className="insideCard">
+                            <p className="color-white">Matte Pomade</p>
+                            <p><span className="color-white">Price: $5.99</span></p>
+                        </div>
+                        <a href="#" id="detailsBtn">Details</a>
+                    </div>
+                </div>
+                <div className="productCard">
+                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
+                    <div className="cardDetails">
+                        <div className="insideCard">
+                            <p className="color-white">Matte Pomade</p>
+                            <p><span className="color-white">Price: $5.99</span></p>
+                        </div>
+                        <a href="#" id="detailsBtn">Details</a>
+                    </div>
+                </div>
+                <div className="productCard">
+                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
+                    <div className="cardDetails">
+                        <div className="insideCard">
+                            <p className="color-white">Matte Pomade</p>
+                            <p><span className="color-white">Price: $5.99</span></p>
+                        </div>
+                        <a href="#" id="detailsBtn">Details</a>
+                    </div>
+                </div>
+                <div className="productCard">
+                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
+                    <div className="cardDetails">
+                        <div className="insideCard">
+                            <p className="color-white">Matte Pomade</p>
+                            <p><span className="color-white">Price: $5.99</span></p>
+                        </div>
+                        <a href="#" id="detailsBtn">Details</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </main>
+    );
+}
