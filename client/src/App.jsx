399eab7e5db5f1b0837ac92a0f2f7ce3b287aa28
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-12 20:57:54 +0200

    +router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/Header/Header.jsx'
-import Home from './components/Home/Home.jsx';
+import Home from './components/Home Template/Home.jsx';
+import Services from './components/Services Template/Services.jsx';
 
 function App() {
   return (
     <>
     <Header />
-
-    <Home />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/services' element={<Services />} />
+    </Routes>
     </>
   );
 
