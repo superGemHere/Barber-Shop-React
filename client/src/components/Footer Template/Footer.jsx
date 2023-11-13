12dbcb3fa27aa1ff73c5cc07923ff86fa78d7156
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-13 23:59:39 +0200

    +div footer

--- client/src/components/Footer Template/Footer.jsx
+++ client/src/components/Footer Template/Footer.jsx
@@ -7,7 +7,9 @@ export default function Footer(){
     return(
         <>
         <footer>
+            <div>
             <span>All rights reserved &copy;</span>
+            </div>
             <span><p>About Us </p></span>
         </footer>
         </>
