6e741774ff96c5fb017fbfb2fbb3b43f43b2a474
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-14 00:14:56 +0200

    + modify footer

--- client/src/components/Footer Template/Footer.jsx
+++ client/src/components/Footer Template/Footer.jsx
@@ -7,11 +7,19 @@ export default function Footer(){
     return(
         <>
         <footer>
+            <div className="links">
+                <div className='aboutUs-div'>
+                <span><p>About Us </p></span>
+                </div>
+                <div className='contancts'>
+                    <p>Contact us</p>
+                </div>
+                <div className='certificate'>
+                    <p>Certificate</p>
+                </div>
+            </div>
             <div className='copyright'>
             <span>All rights reserved &copy;</span>
-            </div>
-            <div className='aboutUs-div'>
-            <span><p>About Us </p></span>
             </div>
         </footer>
         </>
