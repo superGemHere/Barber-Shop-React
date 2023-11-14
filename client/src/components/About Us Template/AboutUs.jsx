867c8a66763164803ad23cfdf1c4469cfcfd3bbb
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-14 22:02:59 +0200

    +about us component
    About

---
+++ client/src/components/About Us Template/AboutUs.jsx
@@ -0,0 +1,33 @@
+import './aboutUs.css'
+
+export default function AboutUs() {
+
+
+    return(
+        <>
+        <main className='about-us'>
+            <div className="content">
+                <div className="container">
+                    <p className="msg">About Us</p>
+                </div>
+                <div className="parent">
+                    <div className="story">
+                        <h2>Our Story</h2>
+                        <div className="storyText">
+                           <p>Compass was founded on the belief that a haircut is not just a service but a personal experience. With 4+ years of dedicated service to our community, we have become more than just a barbershop. We're a destination where style and tradition meet.</p>
+                        </div>
+                    </div>
+                    <div className="experience">
+                        <h2>Years of Experience</h2>
+                    </div>
+                    <div className="experience">
+                        <h2>Location</h2>
+                    </div>
+
+                </div>
+            </div>
+  </main>
+
+        </>
+    );
+}
