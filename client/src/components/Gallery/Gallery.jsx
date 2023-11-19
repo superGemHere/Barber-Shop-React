c74155c74a1176d8f17dec98220162c7fa99fba5
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-19 23:48:21 +0200

    modify gallery component plus css

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,10 +1,14 @@
 import './gallery.css'
 
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 export default function Gallery(){
 
 
     return(
         <main className="gallery">
+            <h1>Compass Gallery</h1>
             <div className="galleryContainer">
                 <div className='photoCard'>
                     <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
@@ -18,6 +22,38 @@ export default function Gallery(){
                     <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
                     <div className='userCredentials'>
                         <p className='photoCaption'>Very good fade asdasdasdasdasdasdasd.</p>
+                        <p className='username'>By: Dzhem Syuleyman</p>
+                        <p className='createdOn'>23.04.2023</p>
+                    </div>
+                </div>
+                <div className='photoCard'>
+                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
+                    <div className='userCredentials'>
+                        <p className='photoCaption'>Very good fade.</p>
+                        <p className='username'>By: Dzhem Syuleyman</p>
+                        <p className='createdOn'>23.04.2023</p>
+                    </div>
+                </div>
+                <div className='photoCard'>
+                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
+                    <div className='userCredentials'>
+                        <p className='photoCaption'>Very good fade.</p>
+                        <p className='username'>By: Dzhem Syuleyman</p>
+                        <p className='createdOn'>23.04.2023</p>
+                    </div>
+                </div>
+                <div className='photoCard'>
+                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
+                    <div className='userCredentials'>
+                        <p className='photoCaption'>Very good fade.</p>
+                        <p className='username'>By: Dzhem Syuleyman</p>
+                        <p className='createdOn'>23.04.2023</p>
+                    </div>
+                </div>
+                <div className='photoCard'>
+                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
+                    <div className='userCredentials'>
+                        <p className='photoCaption'>Very good fade.</p>
                         <p className='username'>By: Dzhem Syuleyman</p>
                         <p className='createdOn'>23.04.2023</p>
                     </div>
