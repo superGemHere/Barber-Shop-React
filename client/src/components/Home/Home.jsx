13b70936b3e7ecee5278733728a947eb7989f0d1
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-19 13:34:44 +0200

    modify Home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,4 +1,7 @@
 import './home.css'
+
+import {Link} from 'react-router-dom'
+
 export default function Home(){
     return ( 
         <main id="home">
@@ -9,7 +12,13 @@ export default function Home(){
         We are #1 Barber Shop and this is possible because of our satisfied
         clients.
       </h4>
-      <p />
+      <div className='home-buttons'>
+        {/* <h2>Check out our</h2> */}
+        <div className='home-button-links'>
+      <Link className='buttons'  to='/services'>Services</Link>
+      <Link className='buttons'  to='/Products'>Products</Link>
+      </div>
+      </div>
     </div>
   </div>
   {/* <h1>Hello</h1> */}
