be89bc4c0d6bcfb220de9f139d5e03d485a6d770
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-22 19:03:32 +0200

    +formating

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -13,7 +13,7 @@ export default function useForm(submitHandler, initialValues){
     const onSubmit = (e) => {
         e.preventDefault();
 
-        submitHandler(values)
+        submitHandler(values);
     }
     
     return{
