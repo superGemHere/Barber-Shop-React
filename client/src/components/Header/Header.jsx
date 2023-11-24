dbc2fa52eb2ecf25bf1ee881ed0bb28b80e97c8b
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-24 14:43:57 +0200

    Dynamic Nav

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,35 +1,72 @@
-import './header.css';
+import "./header.css";
 
-import {Link} from 'react-router-dom'
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../../contexts/authContext";
 
-export default function Header(){
-    return (
-        <header className="header">
-  <a className="profile-link" href="#">
-    Profile
-  </a>
-  <nav className="navbar">
-    <div className="interaction-links">
-      <Link className='navbar__item'  to='/'>Home</Link>
+export default function Header() {
+  const { isAuthenticated } = useContext(AuthContext);
+  return (
+    <header className="header">
+      {isAuthenticated && (
+        <a className="profile-link" href="#">
+        Profile
+      </a>
+      )}
+      
+      <nav className="navbar">
+        <div className="interaction-links">
+          <Link className="navbar__item" to="/">
+            Home
+          </Link>
 
-      <Link className='navbar__item'  to='/gallery'>Gallery</Link>
-      {/* <a className="navbar__item" href="#">
+          <Link className="navbar__item" to="/gallery">
+            Gallery
+          </Link>
+
+          {/* <a className="navbar__item" href="#">
         Feedback
       </a> */}
-      <Link className='navbar__item'  to='/services'>Services</Link>
-      <Link className='navbar__item'  to='/products'>Products</Link>
+          <Link className="navbar__item" to="/services">
+            Services
+          </Link>
+          <Link className="navbar__item" to="/products">
+            Products
+          </Link>
 
+          {isAuthenticated &&
+            <Link className="navbar__item" to="/add-photo">
+              Add Photo
+            </Link>}
+        </div>
+        {isAuthenticated &&
+          <div className="user-links">
+            <a id="userLogout" href="#">
+              Logout
+            </a>
+          </div>}
+        {!isAuthenticated &&
+          <div className="user-links">
+            <Link className="navbar__item" to="/login">
+              Login
+            </Link>
+            <Link className="navbar__item" to="/register">
+              Register
+            </Link>
+          </div>}
 
-      <Link className='navbar__item'  to='/add-photo'>Add Photo</Link>
-    </div>
-    <div className="user-links">
-    <Link className='navbar__item'  to='/login'>Login</Link>
-      <Link className='navbar__item'  to='/register'>Register</Link>
-      <a id="userLogout" href="#">
-        Logout
-      </a>
-    </div>
-  </nav>
-</header>
-    );
+        {/* <div className="user-links">
+          <Link className="navbar__item" to="/login">
+            Login
+          </Link>
+          <Link className="navbar__item" to="/register">
+            Register
+          </Link>
+          <a id="userLogout" href="#">
+            Logout
+          </a>
+        </div> */}
+      </nav>
+    </header>
+  );
 }
