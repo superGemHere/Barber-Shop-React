7a1901615763038f758f2effe71814798494031e
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-25 16:25:42 +0200

    modify css

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,30 +1,31 @@
-import * as request from '../lib/request'
+import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/users'
+const baseUrl = "http://localhost:3030/users";
 
+export const login = async (email, password) => {
+  const result = await request.post(`${baseUrl}/login`, {
+    email,
+    password
+  });
 
-export const  login = async(email, password) => {
-    const result = await request.post(`${baseUrl}/login`, {
-        email,
-        password
-    })
-
-    return result;
-}
+  return result;
+};
 
 export const register = (firstName, lastName, email, password) => {
-    console.log(firstName)
-    console.log(lastName)
-    console.log(email)
-    console.log(password)
- const res = request.post(`${baseUrl}/register`, {
+  // console.log(firstName)
+  // console.log(lastName)
+  // console.log(email)
+  // console.log(password)
+  const res = request.post(`${baseUrl}/register`, {
     firstName,
     lastName,
     email,
     password
-})
+  });
 
-return res;
+  return res;
 
-// console.log(res)
-}
+  // console.log(res)
+};
+
+export const logout = () => request.get(`${baseUrl}/logout`);
