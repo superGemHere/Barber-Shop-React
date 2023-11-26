321e7a1b188819b8cfdb914e25615fb12e15352a
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-26 19:12:21 +0200

    Update authContext.jsx

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,4 +1,9 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import * as authService from '../services/authService.js'
+import Path from "../paths";
+import usePersistedState from "../hooks/userPersistedState.js";
 
 const AuthContext = createContext();
 
@@ -6,12 +11,50 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({
     children,
-    value
 }) => {
     
+    const navigate = useNavigate();
+    const [auth, setAuth] = usePersistedState('auth', {});
+  
+  
+    const loginSubmitHandler = async(values) => {
+      const result = await authService.login(values.email, values.password);
+  
+      setAuth(result);
+      localStorage.setItem('accessToken', result.accessToken)
+  
+      navigate(Path.Home)
+    }
+  
+    const registerSubmitHandler = async (values) =>{
+      console.log(values)
+      const result = await authService.register(values.firstName, values.lastName, values.email, values.password);
+  
+      setAuth(result)
+      localStorage.setItem('accessToken', result.accessToken)
+  
+      navigate(Path.Home)
+    }
+  
+    const logoutHandler = () => {
+      setAuth({})
+      localStorage.removeItem('accessToken')
+  
+      navigate(Path.Home)
+  
+    }
+  
+    const values = {
+      loginSubmitHandler,
+      registerSubmitHandler,
+      logoutHandler,
+      email: auth.email,
+      isAuthenticated: !!auth.accessToken,
+    }
+  
     
     return (
-        <AuthContext.Provider value={value}>
+        <AuthContext.Provider value={values} >
             {children}  
         </AuthContext.Provider>
     )
