fb5031da11f7acab14f8e2e63aa79301cebfa1f6
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-27 23:17:28 +0200

    add dynamic data

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,77 +1,47 @@
 import './gallery.css'
 
+import { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+
+import * as postService from '../../services/postService';
+
+
 
 export default function Gallery(){
-
+    const [photos, setPhotos] = useState([]);
+    useEffect( () => {
+       
+    postService.getAllPhotos()
+    .then((result) => {
+        setPhotos(result)
+        
+        })
+    
+    }, []);
+       
+    const isEmpty = photos.length == 0 ? true : false;
 
     return(
+        
         <main className="gallery">
             <h1>Compass Gallery</h1>
             <div className="galleryContainer">
-                <div className='photoCard'>
-                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
+            {!isEmpty && photos.map(currentPhoto => {
+                return(
+                <div className='photoCard' key={currentPhoto._id} >
+                    <img className='image' src={currentPhoto.imageUrl} alt="" />
                     <div className='userCredentials'>
-                        <p className='photoCaption'>Awesome haircut, you should try at least once and I guarantee you will be amazed.</p>
-                        <p className='username'>By: Dzhem Syuleyman</p>
-                        <p className='createdOn'>23.04.2023</p>
+                        <p className='photoCaption'>{currentPhoto.caption}</p>
+                        <p className='username'>By: {currentPhoto.ownerName}</p>
+                        <p className='createdOn'>{currentPhoto._createdOn}</p>
+                    <Link to={`/gallery/details/${currentPhoto._id}`} className='detailsBtnGallery'>Details</Link>
                     </div>
-                </div>
-                <div className='photoCard'>
-                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
-                    <div className='userCredentials'>
-                        <p className='photoCaption'>Very good fade asdasdasdasdasdasdasd.</p>
-                        <p className='username'>By: Dzhem Syuleyman</p>
-                        <p className='createdOn'>23.04.2023</p>
                     </div>
-                </div>
-                <div className='photoCard'>
-                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
-                    <div className='userCredentials'>
-                        <p className='photoCaption'>Very good fade.</p>
-                        <p className='username'>By: Dzhem Syuleyman</p>
-                        <p className='createdOn'>23.04.2023</p>
-                    </div>
-                </div>
-                <div className='photoCard'>
-                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
-                    <div className='userCredentials'>
-                        <p className='photoCaption'>Very good fade.</p>
-                        <p className='username'>By: Dzhem Syuleyman</p>
-                        <p className='createdOn'>23.04.2023</p>
-                    </div>
-                </div>
-                <div className='photoCard'>
-                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
-                    <div className='userCredentials'>
-                        <p className='photoCaption'>Very good fade.</p>
-                        <p className='username'>By: Dzhem Syuleyman</p>
-                        <p className='createdOn'>23.04.2023</p>
-                    </div>
-                </div>
-                <div className='photoCard'>
-                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
-                    <div className='userCredentials'>
-                        <p className='photoCaption'>Very good fade.</p>
-                        <p className='username'>By: Dzhem Syuleyman</p>
-                        <p className='createdOn'>23.04.2023</p>
-                    </div>
-                </div>
-                <div className='photoCard'>
-                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
-                    <div className='userCredentials'>
-                        <p className='photoCaption'>Very good fade.</p>
-                        <p className='username'>By: Dzhem Syuleyman</p>
-                        <p className='createdOn'>23.04.2023</p>
-                    </div>
-                </div>
-                <div className='photoCard'>
-                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibHRK8mA1q1TUOYt9jvsiYDG4oLVbJ_rfDQ&usqp=CAU" alt="" />
-                    <div className='userCredentials'>
-                        <p className='photoCaption'>Very good fade.</p>
-                        <p className='username'>By: Dzhem Syuleyman</p>
-                        <p className='createdOn'>23.04.2023</p>
-                    </div>
-                </div>
+                
+                )
+                
+            })}
+            {isEmpty && <h3>There are no photos to display</h3>}
             </div>
         </main>
     );
