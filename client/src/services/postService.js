6d70248f52e3283f154853258cac6e29c51446dc
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-12-01 14:32:08 +0200

    add delete function

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -2,30 +2,31 @@ import * as request from "../lib/request";
 
 const baseUrl = "http://localhost:3030/data";
 
+export const addPhoto = async (imageUrl, caption, ownerName) => {
+  const result = await request.post(`${baseUrl}/photos`, {
+    imageUrl,
+    caption,
+    ownerName
+  });
+  console.log(result);
+  return result;
+};
 
+export const getAllPhotos = async () => {
+  const result = await request.get(`${baseUrl}/photos`);
 
-export const addPhoto = async(imageUrl, caption, ownerName) => {
-    const result = await request.post(`${baseUrl}/photos`, {
-        imageUrl,
-        caption,
-        ownerName,
-    });
-    console.log(result)
-    return result;
-}
+  return result;
+};
+export const getOnePhotos = async photoId => {
+  const result = await request.get(`${baseUrl}/photos/${photoId}`);
 
-export const getAllPhotos = async() => {
-    const result = await request.get(`${baseUrl}/photos`);
+  return result;
+};
+export const updatePhoto = async (photoId, photoData) => {
+  const result = await request.put(`${baseUrl}/photos/${photoId}`, photoData);
 
-    return result;
-}
-export const getOnePhotos = async(photoId) => {
-    const result = await request.get(`${baseUrl}/photos/${photoId}`);
-
-    return result;
-}
-export const updatePhoto =  async (photoId, photoData) => {
-    const result = await request.put(`${baseUrl}/photos/${photoId}`, photoData)
-
-    return result;
-}
+  return result;
+};
+export const deletePhoto = async photoId => {
+  await request.remove(`${baseUrl}/photos/${photoId}`);
+};
