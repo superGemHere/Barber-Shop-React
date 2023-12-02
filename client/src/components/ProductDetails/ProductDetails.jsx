7a02eb671a9bc77c3c12af49d5fe73afdc691151
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-12-02 19:43:15 +0200

    create productDetails

---
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,50 @@
+import './productDetails.css'
+
+import { useContext, useEffect, useState } from 'react';
+import {Link, useParams, useNavigate} from 'react-router-dom';
+
+
+import * as productService from '../../services/productService';
+import AuthContext from '../../contexts/authContext';
+
+
+export default function ProductDetails(){
+    const {userId, isAdmin} = useContext(AuthContext)
+    const [product, setProduct] = useState({});
+    const {productId} = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        productService.getOneProduct(productId)
+        .then(result => setProduct(result))
+    }, [productId]);
+
+    
+
+    const deleteHandler = async () => {
+        if(confirm('Are you sure you want to delete this product?')){
+            await productService.deleteProduct(productId);
+            navigate('/products');
+        }
+    }
+
+    const isOwner = product._ownerId === userId;
+
+
+
+    return(
+        <main className='productDetails'>
+            <div className="productContainerDetails">
+                <img src={product.productImageUrl} alt="" />
+                <div className='oneProductInsights'>
+                <h2>{product.productName}</h2>
+                <h3>{product.productBrand}</h3>
+                <p>{product.productDescription}</p>
+                <p>Price: ${product.productPrice}</p>
+            </div>
+                {isAdmin && <button className='productDetailsDeleteBtn' onClick={() => deleteHandler(product._id)}>Delete</button>}
+                </div>
+            <p className='buyPolicy'>Products can't be ordered online, you can buy them only from the physical barber shop.</p>
+        </main>
+    );
+}
