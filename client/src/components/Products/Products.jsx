caa96fa731cec475fb7f54a1d53286067de3cbda
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-12-02 19:43:30 +0200

    Update Products.jsx

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,84 +1,50 @@
 import './products.css'
 
+import { useState, useEffect } from "react";
+import { Link, useNavigate} from 'react-router-dom';
+
+import * as productService from '../../services/productService';
+
 export default function Products(){
 
+    const [products, setProducts] = useState([]);
+    const navigate = useNavigate();
+    useEffect( () => {
+       
+        productService.getAllProducts()
+    .then((result) => {
+        setProducts(result)
+        console.log(result)
+        })
+    
+    }, []);
+       
+    
+    const isEmpty = products.length == 0 ? true : false;
+
+    console.log(isEmpty)
 
     return(
         <main className="products">
         <div className="productsParentContainer">
-            <h2 id="productsHeading">Our Products:</h2>
+            {/* <h2 id="productsHeading">Products</h2> */}
             <div className="productChildContainer">
-                <div className="productCard">
-                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
-                    <div className="cardDetails">
-                        <div className="insideCard">
-                            <p className="color-white">Matte Pomade</p>
-                            <p><span className="color-white">Price: $5.99</span></p>
+                {!isEmpty && products.map(currentProduct => {
+                    return(
+                        <div className="productCard">
+                            <img src={currentProduct.productImageUrl} alt={currentProduct.productName} />
+                            <div className="cardDetails">
+                            <div className="insideCard">
+                                <p className="color-white">{currentProduct.productName}</p>
+                                <p><span className="color-white">Price: ${currentProduct.productPrice}</span></p>
+                            </div>
+                                <Link to={`/products/details/${currentProduct._id}`} id="detailsBtn">Details</Link>
+                            </div>
                         </div>
-                        <a href="#" id="detailsBtn">Details</a>
-                    </div>
-                </div>
-                <div className="productCard">
-                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
-                    <div className="cardDetails">
-                        <div className="insideCard">
-                            <p className="color-white">Matte Pomade</p>
-                            <p><span className="color-white">Price: $5.99</span></p>
-                        </div>
-                        <a href="#" id="detailsBtn">Details</a>
-                    </div>
-                </div>
-                <div className="productCard">
-                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
-                    <div className="cardDetails">
-                        <div className="insideCard">
-                            <p className="color-white">Matte Pomade</p>
-                            <p><span className="color-white">Price: $5.99</span></p>
-                        </div>
-                        <a href="#" id="detailsBtn">Details</a>
-                    </div>
-                </div>
-                <div className="productCard">
-                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
-                    <div className="cardDetails">
-                        <div className="insideCard">
-                            <p className="color-white">Matte Pomade</p>
-                            <p><span className="color-white">Price: $5.99</span></p>
-                        </div>
-                        <a href="#" id="detailsBtn">Details</a>
-                    </div>
-                </div>
-                <div className="productCard">
-                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
-                    <div className="cardDetails">
-                        <div className="insideCard">
-                            <p className="color-white">Matte Pomade</p>
-                            <p><span className="color-white">Price: $5.99</span></p>
-                        </div>
-                        <a href="#" id="detailsBtn">Details</a>
-                    </div>
-                </div>
-                <div className="productCard">
-                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
-                    <div className="cardDetails">
-                        <div className="insideCard">
-                            <p className="color-white">Matte Pomade</p>
-                            <p><span className="color-white">Price: $5.99</span></p>
-                        </div>
-                        <a href="#" id="detailsBtn">Details</a>
-                    </div>
-                </div>
-                <div className="productCard">
-                    <img src="https://i00.eu/img/711/1024x1024/ex7ssuk0/16041.jpg" alt="Product Image"/>
-                    <div className="cardDetails">
-                        <div className="insideCard">
-                            <p className="color-white">Matte Pomade</p>
-                            <p><span className="color-white">Price: $5.99</span></p>
-                        </div>
-                        <a href="#" id="detailsBtn">Details</a>
-                    </div>
-                </div>
-
+                    );
+                })}
+                
+                {isEmpty && <h1 className='zeroProducts'>There are no available products</h1>}
             </div>
         </div>
     </main>
