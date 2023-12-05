e08f13db6b8cbaa625589946a68ab79cfcc4a879
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-12-05 21:06:45 +0200

    Create Haircuts.jsx

---
+++ client/src/components/ServiceViews/Haircuts.jsx
@@ -0,0 +1,77 @@
+import './serviceViews.css'
+
+export default function Haircut(){
+
+
+    return(
+        <main className="servicePriceList">
+            <h1>Haircut Prices</h1>
+            <div className='haircutServiceParent'>
+            <div className="barberContainer1">
+                
+                <div className="ball1"></div>
+                                  
+                <div className="endCap1"></div>
+                                  
+                <div className="cap1"></div>
+                                  
+                <div className="barberWrap1">
+                                  
+                  <div className="barber1"></div>
+                                  
+                </div>
+                                  
+                <div className="cap1"></div>
+                                  
+                <div className="endCap1"></div>
+                                  
+                <div className="ball1"></div>
+                              
+              </div>
+            <div className="servicePriceListInsides">
+                <div className="typeInsides">
+                    <p>Military Cut - $7 </p>
+                    <p className='typeInsides-SecondChild'>( Includes only hair trimming )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Normal Cut - $10 </p>
+                    <p className='typeInsides-SecondChild'>( Includes only hair trimming )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Fade Haircut- $12 </p>
+                    <p className='typeInsides-SecondChild'>( Includes a fade of your choice )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Taper Haircut - $15 </p>
+                    <p className='typeInsides-SecondChild'>( Includes taper style of your choice with hair wax and hair wash )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Style by your choice - $17 </p>
+                    <p className='typeInsides-SecondChild'>( Includes style of your choice with hair wax and hair wash )</p>
+                </div>
+            </div>
+            <div className="barberContainer2">
+                
+                <div className="ball2"></div>
+                                  
+                <div className="endCap2"></div>
+                                  
+                <div className="cap2"></div>
+                                  
+                <div className="barberWrap2">
+                                  
+                  <div className="barber2"></div>
+                                  
+                </div>
+                                  
+                <div className="cap2"></div>
+                                  
+                <div className="endCap2"></div>
+                                  
+                <div className="ball2"></div>
+                              
+              </div>
+            </div>
+        </main>
+    );
+}
