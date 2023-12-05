6743aad855d6fc7b6edf9b6c0db1675d3daf321f
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-12-05 21:06:56 +0200

    Update Services.jsx

--- client/src/components/Services/Services.jsx
+++ client/src/components/Services/Services.jsx
@@ -1,42 +1,44 @@
-import './services.css'
+import './services.css';
+
+import {useNavigate} from 'react-router-dom'
 
 export default function Services(){
+    const navigate = useNavigate();
 
     return (
         <>
         <main className="services color-white">
     <div className="servicesParentContainer">
-        <p id="serviceHeading">Here are our services.</p>
+        <p id="serviceHeading">Services</p>
         <div className="servicesChildContainer">
-                <div className="serviceCard haircuts" >
+                <div className="serviceCard haircuts" onClick={() => navigate('/services/haircuts')}>
                                 <h2>Haircuts</h2>
                                 <div className="cardInsights">
-                                    <h4>Haircuts</h4>
-                                    <p>Click for more details.</p>
+                                    {/* <h4>Haircuts</h4> */}
+                                    <p>Click for price list</p>
                                 </div>
                             </div>
             
-            <div className="serviceCard beardTrimming" >
+            <div className="serviceCard beardTrimming" onClick={() => navigate('/services/beard-trimming')}>
                 <h2>Beard Trimming</h2>
                     <div className="cardInsights">
-                        <h4>Beard Trimming</h4>
-                        <p>Click for more details.</p>
+                        {/* <h4>Beard Trimming</h4> */}
+                        <p>Click for price list</p>
                     </div>
             </div>
-            <div className="serviceCard wax" >
-                <h2>Wax</h2>
+            <div className="serviceCard wax" onClick={() => navigate('/services/other-services')}>
+                <h2>Other Services</h2>
                     <div className="cardInsights">
-                        <h4>Wax</h4>
-                        <p>Click for more details.</p>
+                        {/* <h4>Wax</h4> */}
+                        <p>Click for price list</p>
                     </div>
             </div>
-            <div className="serviceCard masks">
+            {/* <div className="serviceCard masks">
                 <h2>Face Masks</h2>
                     <div className="cardInsights">
-                        <h4>Face Masks</h4>
-                        <p>Click for more details.</p>
+                        <p>Click for price list</p>
                     </div>
-            </div>
+            </div> */}
         </div>
     </div>
 </main>
