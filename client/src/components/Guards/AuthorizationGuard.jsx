bb1dfa8140f498cf194f97f31a10c058d2c92de3
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-12-05 21:03:16 +0200

    Update AuthorizationGuard.jsx

--- client/src/components/Guards/AuthorizationGuard.jsx
+++ client/src/components/Guards/AuthorizationGuard.jsx
@@ -1,27 +1,34 @@
 import { useContext, useEffect, useState } from "react";
-import { Navigate, Outlet, useParams } from "react-router-dom";
+import { Navigate, Outlet, useNavigate, useParams } from "react-router-dom";
 
 import AuthContext from "../../contexts/authContext";
 
 import * as postService from '../../services/postService'
 
 
+
 export default function AuthorizationGuard(props){
     const {photoId} = useParams();
-    const [photo, setPhoto] = useState({})
+    const navigate = useNavigate();
     const { userId } = useContext(AuthContext);
+    const [photo, setPhoto] = useState({});
+
 
     useEffect(() => {
         postService.getOnePhotos(photoId)
-        .then(result => {
+        .then((result) => {
             setPhoto(result);
+            console.log(result)
         })
-    }, [])
+        .catch(err => console.log(err))
+    }, [photoId]);
 
-    if(photo._ownerId !== userId){
-        return <Navigate to='/gallery' />;
+
+    console.log(photo)
+    if(photo._ownerId != userId){
+        return navigate('/gallery');
     }
-    
+
 
     return <Outlet />
 }
