6d9e66c822c2ab2f4cf4bcf55b74e70de98f0a6f
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-12-05 21:06:54 +0200

    Create OtherServices.jsx

---
+++ client/src/components/ServiceViews/OtherServices.jsx
@@ -0,0 +1,81 @@
+import './serviceViews.css'
+
+export default function OtherServices(){
+
+
+    return(
+        <main className="servicePriceList">
+            <h1>Haircut Prices</h1>
+            <div className='haircutServiceParent'>
+            <div className="barberContainer1">
+                
+                <div className="ball1"></div>
+                                  
+                <div className="endCap1"></div>
+                                  
+                <div className="cap1"></div>
+                                  
+                <div className="barberWrap1">
+                                  
+                  <div className="barber1"></div>
+                                  
+                </div>
+                                  
+                <div className="cap1"></div>
+                                  
+                <div className="endCap1"></div>
+                                  
+                <div className="ball1"></div>
+                              
+              </div>
+            <div className="servicePriceListInsides">
+                <div className="typeInsides">
+                    <p>Eyebrow Shapping- $2 </p>
+                    <p className='typeInsides-SecondChild'>( Includes eyebrow shapping )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Wax - $3 </p>
+                    <p className='typeInsides-SecondChild'>( Includes only wax )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Nose & Ear grooming - $5 </p>
+                    <p className='typeInsides-SecondChild'>( Includes Nose & Ear grooming )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Beard Care - $10 </p>
+                    <p className='typeInsides-SecondChild'>( Includes beard washing with nourishing shampoo and conditioning with special balm plus beard oil for softening your beard )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Hair Care - $12</p>
+                    <p className='typeInsides-SecondChild'>( Includes hair washing with nourishing shampoo and conditioning with special balm plus hair tonic for relaxing your hair )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Hair Dyeing - $20</p>
+                    <p className='typeInsides-SecondChild'>( Includes only hair dyeing )</p>
+                </div>
+            </div>
+            <div className="barberContainer2">
+                
+                <div className="ball2"></div>
+                                  
+                <div className="endCap2"></div>
+                                  
+                <div className="cap2"></div>
+                                  
+                <div className="barberWrap2">
+                                  
+                  <div className="barber2"></div>
+                                  
+                </div>
+                                  
+                <div className="cap2"></div>
+                                  
+                <div className="endCap2"></div>
+                                  
+                <div className="ball2"></div>
+                              
+              </div>
+            </div>
+        </main>
+    );
+}
