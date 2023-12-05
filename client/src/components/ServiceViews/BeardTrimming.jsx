79fa2acfdd9a6c87cb728da5f4b2d11b64c944c9
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-12-05 21:06:50 +0200

    Create BeardTrimming.jsx

---
+++ client/src/components/ServiceViews/BeardTrimming.jsx
@@ -0,0 +1,73 @@
+import './serviceViews.css'
+
+export default function BeardTrimming(){
+
+
+    return(
+        <main className="servicePriceList">
+            <h1>Beard Trimming Prices</h1>
+            <div className='haircutServiceParent'>
+            <div className="barberContainer1">
+                
+                <div className="ball1"></div>
+                                  
+                <div className="endCap1"></div>
+                                  
+                <div className="cap1"></div>
+                                  
+                <div className="barberWrap1">
+                                  
+                  <div className="barber1"></div>
+                                  
+                </div>
+                                  
+                <div className="cap1"></div>
+                                  
+                <div className="endCap1"></div>
+                                  
+                <div className="ball1"></div>
+                              
+              </div>
+            <div className="servicePriceListInsides">
+                <div className="typeInsides">
+                    <p>Smooth Cheeks - $5 </p>
+                    <p className='typeInsides-SecondChild'>( Includes only shaving and after shave cologne )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Soft Cheeks - $7 </p>
+                    <p className='typeInsides-SecondChild'>( Includes shaving and after shave balm to soften your skin )</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Stylish Beard - $10 </p>
+                    <p className='typeInsides-SecondChild'>( Includes a style of your choice beard trimming)</p>
+                </div>
+                <div className="typeInsides">
+                    <p>Alpha Trim - $12 </p>
+                    <p className='typeInsides-SecondChild'>( Includes a style of your choice beard trimming with а nourishing beard balm )</p>
+                </div>
+            </div>
+            <div className="barberContainer2">
+                
+                <div className="ball2"></div>
+                                  
+                <div className="endCap2"></div>
+                                  
+                <div className="cap2"></div>
+                                  
+                <div className="barberWrap2">
+                                  
+                  <div className="barber2"></div>
+                                  
+                </div>
+                                  
+                <div className="cap2"></div>
+                                  
+                <div className="endCap2"></div>
+                                  
+                <div className="ball2"></div>
+                              
+              </div>
+            </div>
+        </main>
+    );
+}
