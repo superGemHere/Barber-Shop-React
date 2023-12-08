3b33b5ef40090eac2ce1bd510e34c4ceb25e25c6
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-12-08 14:27:20 +0200

    Update Services.jsx

--- client/src/components/Services/Services.jsx
+++ client/src/components/Services/Services.jsx
@@ -9,12 +9,10 @@ export default function Services(){
         <>
         <main className="services color-white">
     <div className="servicesParentContainer">
-        <p id="serviceHeading">Services</p>
         <div className="servicesChildContainer">
                 <div className="serviceCard haircuts" onClick={() => navigate('/services/haircuts')}>
                                 <h2>Haircuts</h2>
                                 <div className="cardInsights">
-                                    {/* <h4>Haircuts</h4> */}
                                     <p>Click for price list</p>
                                 </div>
                             </div>
@@ -22,23 +20,15 @@ export default function Services(){
             <div className="serviceCard beardTrimming" onClick={() => navigate('/services/beard-trimming')}>
                 <h2>Beard Trimming</h2>
                     <div className="cardInsights">
-                        {/* <h4>Beard Trimming</h4> */}
                         <p>Click for price list</p>
                     </div>
             </div>
             <div className="serviceCard wax" onClick={() => navigate('/services/other-services')}>
                 <h2>Other Services</h2>
                     <div className="cardInsights">
-                        {/* <h4>Wax</h4> */}
                         <p>Click for price list</p>
                     </div>
             </div>
-            {/* <div className="serviceCard masks">
-                <h2>Face Masks</h2>
-                    <div className="cardInsights">
-                        <p>Click for price list</p>
-                    </div>
-            </div> */}
         </div>
     </div>
 </main>
